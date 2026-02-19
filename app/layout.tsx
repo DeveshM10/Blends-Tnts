9f5bfaff896362f4338adb299547dc7e348203b1
DeveshM10 <mamadapurdevesh@gmail.com>
2026-02-20 02:53:08 +0530

    Update website title and metadata to TNTS X Blends

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'TNTS X Blends',
+  description: 'National Youth Summit & MUN Conference 2026',
   generator: 'v0.app',
   icons: {
     icon: [
